de2d2c4d8c788422eafcdebf0b952017051dbec1
leahgoldberg <leahgoldberg31@gmail.com>
2015-10-10 18:40:00 -0400

    complete method to pull top three preferred brands per user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
 	has_many :transactions
 
 	def preferred_brands
-		
+		transactions.group('vendor').order('count_id desc').count('id').map {|k,v| k}.first(3)
 	end	
 end
