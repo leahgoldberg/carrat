cef63534b36ad75195e3f6e4eab99a793983160c
leahgoldberg <leahgoldberg31@gmail.com>
2015-10-10 15:59:04 -0400

    add welcome controller with index action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,16 @@
-class WelcomeControllerController < ApplicationController
+class WelcomeController < ApplicationController
+
+	def index
+		@current_user = current_user
+		@top_challenges = Curatable.curate_challenges(current_user)
+		@top_rewards = Curatable.curate_rewards(current_user)
+		@trusted_brands = current_user.trusted_brands
+	end
+
+	private
+
+	def current_user
+		User.find_by(username: "Morgan Stanley")
+	end	
+		
 end
