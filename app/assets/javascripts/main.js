84af66b6899ad2433b9d20de71634ede31e82d97
GLNRO <lg@glnro.com>
2015-10-11 01:21:26 -0400

    Add ajax call to sqoot api

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
 	alert("HI");
   $(document).foundation();
+
+
+  $.ajax({
+    type: "GET",
+    url:"http://api.sqoot.com/v2/deals?api_key=6jfpwl&callback=women_fashion"}
+    ).done(function(response){
+      console.log(response)
+    })
 });
