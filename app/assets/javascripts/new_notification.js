5413ec0cb59844f89d754dd733387abad8c6cfc7
leahgoldberg <leahgoldberg31@gmail.com>
2015-10-11 14:17:36 -0400

    update total carrat points when new notification appears

--- app/assets/javascripts/new_notification.js
+++ app/assets/javascripts/new_notification.js
@@ -1,5 +1,8 @@
 function initNotifications() {
   $("#notify-trigger").on("click", function() {
     $("#new-notification").fadeIn(1000);
+    setTimeout(function() {
+      $(".user-points h3").text("72400 carrats").fadeIn(1000);
+    }, 300);
   });
 }
