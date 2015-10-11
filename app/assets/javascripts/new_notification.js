14c41974ae076bcecbe4c6d3bb2a75096308cf81
leahgoldberg <leahgoldberg31@gmail.com>
2015-10-11 15:28:38 -0400

    update user carrat points

--- app/assets/javascripts/new_notification.js
+++ app/assets/javascripts/new_notification.js
@@ -2,7 +2,7 @@ function initNotifications() {
   $("#notify-trigger").on("click", function() {
     $("#new-notification").fadeIn(1000);
     setTimeout(function() {
-      $(".user-points h3").text("72900 carrats").fadeIn(1000);
+      $(".user-points h3").text("53500 carrats").fadeIn(1000);
     }, 300);
   });
 }
