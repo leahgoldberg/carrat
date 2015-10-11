746fd6ac2fd1cfd3e4a6431b919cd9daeee31bee
leahgoldberg <leahgoldberg31@gmail.com>
2015-10-10 20:50:10 -0400

    add claimed rewards controller

--- app/controllers/claimed_rewards_controller.rb
+++ app/controllers/claimed_rewards_controller.rb
@@ -1,2 +1,13 @@
 class ClaimedRewardsController < ApplicationController
+
+	def create
+		current_user.claimed_rewards.create(reward: claimed_reward_params[:reward])
+		redirect_to root_path
+	end	
+
+	private
+
+	def claimed_reward_params
+		params.require(:claimed_reward).permit(:reward)
+	end	
 end
