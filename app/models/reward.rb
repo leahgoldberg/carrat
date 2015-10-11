1a991de0770626f2b206b71bcbcb56b40d3a5a05
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-10-11 08:55:59 -0400

    add display methods

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -4,4 +4,23 @@ class Reward < ActiveRecord::Base
 	
 	has_many :claimed_rewards
 	belongs_to :vendor
+
+	def carrats
+		self.price.floor * 100
+	end
+
+	def stripped_title
+		self.title.downcase
+			.gsub(/[(][$]\d+( value)[)]/, '')
+			.gsub(/[(]\d+[%]( off)[)]/, '')
+			.gsub(/[(](up to )\d+[%]( off)[)]/, '')
+			.gsub(/[$]\d+( for)/, '')
+			.gsub(/(up to )\d+[%]( off)/, '')
+			.gsub(/[$]\d+( for )[$]\d+( of)/, '')
+			.gsub(/[$]\d+( for )/, '')
+			.gsub(/[$]\d+( worth of)/, '')
+			.gsub(/^\s+/, '').capitalize			
+	end
 end
+
+
