93c61192e56ab18d2831160c1477b3aec0d63ce0
leahgoldberg <leahgoldberg31@gmail.com>
2015-10-11 08:24:02 -0400

    initialize countdown clocks with random hours between 1 and 12

--- app/assets/javascripts/countdown_clock.js
+++ app/assets/javascripts/countdown_clock.js
@@ -2,7 +2,8 @@ function initClock() {
   var challenges = $("#challenges-list").children().find("div");
   for (var i=0; i < challenges.length; i++) {
     var challengeId = challenges[i].id.slice(16);
-    $("#countdown-clock-"+challengeId).FlipClock(3600, {
+    var randomHours = Math.random() * 45000;
+    $("#countdown-clock-"+challengeId).FlipClock(randomHours, {
       countdown: true
     });
   }  
